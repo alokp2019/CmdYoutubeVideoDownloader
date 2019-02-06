5a38390b6d899014f592d030e5a9ee609af545b5
ALOK <alokp2019@gmail.com>
2019-02-06 23:54:16 +0530

    resolved directing using promises

--- server.js
+++ server.js
@@ -7,35 +7,40 @@ const rl = require('readline');
 
 
 
-    	// Prompting the users to enter command line arguments
+      // Prompting the users to enter command line arguments
    let cmd_prompt = rl.createInterface(process.stdin, process.stdout);
         cmd_prompt.question('Please Input a Youtube URL or a search query\n', (res)=>{
       if(!res)
-   	     return
+         return
 
       query.getInfo(res)
      .then((info)=>{
       return new Promise((resolve,reject)=> { //Promises to check and handle errors
-      	let video_title = info.items[0].fulltitle;
-	    let video_url = info.items[0].webpage_url;
-	    if (!fs.existsSync('video_downloads/')) {
-				// Create the downloads directory
-				fs.mkdirSync('video_downloads');
-			}
+        let video_title = info.items[0].fulltitle;
+      let video_url = info.items[0].webpage_url;
+      if (!fs.existsSync('video_downloads/')) {
+        // Create the downloads directory
+        fs.mkdirSync('video_downloads');
+      }
 
               //downloads the highest quality and names the title in the directory
-			let outputStreams = ytdl(video_url, { quality: 'highest' }).pipe(
-				fs.createWriteStream(`video_downloads/${video_title.replace(/[^0-9A-Za-z\s]+/gim, '')}.mp4`)
-			);
+      let outputStreams = ytdl(video_url, { quality: 'highest' }).pipe(
+        fs.createWriteStream(`video_downloads/${video_title.replace(/[^0-9A-Za-z\s]+/gim, '')}.mp4`)
+      );
+             
+           
 
-			console.log('Downloading  completed.');
-      	resolve();
-      }) 	
+      console.log('Downloading  completed.');
+
+
+
+        resolve();
+      })  
 
        //catching the errors
      }).catch((error)=>{
        console.error(`Error retrieving info from URL.\n${error}`);
-			return;
+      return;
      })
 })
 
@@ -54,5 +59,5 @@ const rl = require('readline');
 
 
 app.listen(3010, ()=>{
-	console.log('listening on the new port');
+  console.log('listening on the new port');
 })
